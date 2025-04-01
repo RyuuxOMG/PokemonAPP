03fcaf179b728e76c85cca9385340a6e83216c62
Juan Manuel Ochoa <juanm.ochoamu@autonoma.edu.co>
2025-03-31 20:57:18 -0500

    Se crea intefaz pokemon fuego

---
+++ src/autonoma/pokemonapp/view/PokemonFuego.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package autonoma.pokemonapp.view;
+
+public interface PokemonFuego {
+    void atacarPunioFuego();
+    void atacarAscuas();
+    void atacarLanzallamas();
+}
