263268e941e97e58102991d0ccdc0afaad078f4d
Juan Manuel Ochoa <juanm.ochoamu@autonoma.edu.co>
2025-03-31 20:57:38 -0500

    Se crea intefaz pokemon planta

---
+++ src/autonoma/pokemonapp/view/PokemonPlanta.java
@@ -0,0 +1,9 @@
+
+package autonoma.pokemonapp.view;
+
+public interface PokemonPlanta {
+    void atacarParalizar();
+    void atacarDrenaje();
+    void atacarHojaAfilada();
+    void atacarLatigoCepa();
+}
